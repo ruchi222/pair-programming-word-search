c2a9d2d4922142bcabca8333d5b99a22a5b4285d
Ruchika Gosain <58868182+ruchi222@users.noreply.github.com>
2021-07-28 16:56:51 -0400

    Update wordsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -3,6 +3,14 @@ const wordSearch = (letters, word) => {
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
-}
-
-module.exports = wordSearch
+const transposed = letters[0].map((item, index) => letters.map(row => row[index]))
+    const verticalJoin = transposed.map(ls => ls.join(''))
+    for (v of verticalJoin) {
+        if (v.includes(word))
+        return true;
+    }
+    return false;
+} 
+// Check to find the word horizontally and vertically
+// Return true if the word is found, and return false if the word is not found
+module.exports = wordSearch;
